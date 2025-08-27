a18a6e118d8b8f622822b3b6fe5e6c0ab9adf334
Chirag Garg <chiraggarg879@gmail.com>
2025-08-27 21:42:00 +0530

    Added backend for school_management

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "Chirag@123cg",
+  password: process.env.DB_PASS || "",
   database: process.env.DB_NAME || "school_management"
 });
 
