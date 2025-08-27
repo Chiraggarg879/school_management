de75164cefc89b072578b569c31f0a0e902bea80
Chirag Garg <chiraggarg879@gmail.com>
2025-08-27 23:22:33 +0530

    Added backend for school_management

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "root", 
+  password: process.env.MYSQL_PASSWORD || "Chirag@123cg",
+  database: process.env.MYSQL_DATABASE || "school_management",
   port: process.env.MYSQL_PORT || 3306
 });
 
